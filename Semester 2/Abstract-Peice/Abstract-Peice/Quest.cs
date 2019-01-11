fedaf185961ceb22462e1eb6dac9e8c2ab7bcab9
RussAMR <195712@student.asd20.org>
2019-01-11 14:39:43 -0700

    Uncompleted Abstract testing.

---
+++ Semester 2/Abstract-Peice/Abstract-Peice/Quest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Peice
+{
+    abstract class Quest
+    {
+        protected bool isCompleted;
+        protected bool isHidden;
+        protected bool isActive;
+        protected bool isunlocked;
+        protected bool isLevel_Locked;
+        public Quest(bool isCompleted, bool isHidden, bool isActive, bool isunlocked)
+        {
+            this.isActive = isActive;
+            this.isHidden = isHidden;
+            this.isunlocked = isunlocked;
+            this.isCompleted = isCompleted;
+        }
+        public virtual void Islevellocked()
+        {
+            Console.WriteLine("Level-Locked");
+        }
+    }
+}
