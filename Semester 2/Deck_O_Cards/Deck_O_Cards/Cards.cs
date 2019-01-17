3220efe23bf5129e51fa26078bf9630fdb476d5d
RussAMR <195712@student.asd20.org>
2019-01-17 13:54:40 -0700

    Started Deck Of Cards

---
+++ Semester 2/Deck_O_Cards/Deck_O_Cards/Cards.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck_O_Cards
+{
+    enum Suit
+    {
+        Hearts,
+        Clubs,
+        Spades,
+        Diamonds
+    }
+    enum Face
+    {
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten,
+        J,
+        Q,
+        K,
+        A
+    }
+    class Cards
+    {
+
+        public Face face;
+        public Suit suit;
+        public Cards(Face face, Suit suit)
+        {
+            this.face = face;
+            this.suit = suit;
+            
+        }
+        public void Print(Face face, Suit suit)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int d = 0; d < 11; d++)
+                    {
+                    Console.WriteLine("____________________");
+                    Console.WriteLine("|" + Face[d] + Suit[i] + "|");
+                    Console.WriteLine("____________________");
+                }
+            }
+
+        }
+    }
+
+}
---
+++ Semester 2/Deck_O_Cards/Deck_O_Cards/Deck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck_O_Cards
+{
+    class Deck
+    {
+        List<Cards> deck = new List<Cards>();
+        public Deck()
+        {
+
+            foreach (var face in Enum.GetValues(typeof(Face)))
+            {
+                foreach (var suit in Enum.GetValues(typeof(Suit)))
+                {
+                    deck.Add(new Cards((Face)face, (Suit)suit));
+                }
+            }
+        }
+        public void Shuffle()
+        {
+            for (int i = 0; i < deck.Count - 1; i++)
+            {
+                Random rand = new Random();
+                int Rholder = rand.Next(0 - 51);
+                Cards holder = deck[Rholder];
+                deck.RemoveAt(Rholder);
+                deck.Add(holder);
+            }
+        }
+        public Cards Draw()
+        {
+            if (deck.Count > 0)
+            {
+                Cards myint;
+                myint = deck.Last();
+                deck.RemoveAt(deck.Count() - 1);
+                return myint;
+            }
+            else
+            {
+                Console.WriteLine("Stack Underflow (No values left in the list)");
+                return deck[0];
+            }
+        }
+    }
+
+}
