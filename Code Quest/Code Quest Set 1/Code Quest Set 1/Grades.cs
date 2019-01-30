b053551ed28688577f523178a153fb6c20bd314f
RussAMR <195712@student.asd20.org>
2019-01-30 14:26:54 -0700

    Code Quest attempts Ryan Moore / Andrew Russell

---
+++ Code Quest/Code Quest Set 1/Code Quest Set 1/Grades.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Quest_Set_1
+{
+    class Grades
+    {
+        public int grades1;
+        public int grades2;
+        public int grades3;
+        public int grades4;
+        public int grades5;
+        public int grades6;
+
+        public Grades(int grades1 = 0, int grades2 = 0, int grades3 = 0, int grades4 = 0, int grades5 = 0, int grades6 = 0)
+        {
+            this.grades1 = grades1;
+            this.grades2 = grades2;
+            this.grades3 = grades3;
+            this.grades4 = grades4;
+            this.grades5 = grades5;
+            this.grades6 = grades6;
+        }
+    }
+}
---
+++ Code Quest/Code Quest Set 1/Code Quest Set 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Code_Quest_Set_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grades grades = new Grades();
+            List<Grades> CallItWhatever = new List<Grades>();
+            int myint;
+            string path = AppDomain.CurrentDomain.BaseDirectory + (@"Prob01.in.txt");
+            using (StreamReader sw = new StreamReader(path))
+            {
+                Grades.grades1 = sw.ReadLine();
+            }
+
+            
+
+        }
+    }
+}
