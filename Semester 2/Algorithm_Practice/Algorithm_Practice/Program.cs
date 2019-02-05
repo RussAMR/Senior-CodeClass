949d4f8d8533d68ae4a9f749c14afd028a1ec4d6
RussAMR <195712@student.asd20.org>
2019-02-05 14:39:58 -0700

    Started Alogithm

---
+++ Semester 2/Algorithm_Practice/Algorithm_Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Prob01.in_.txt";
+            int myint;
+            using (StreamReader sw =  new StreamReader(path))
+            {
+                myint = int.Parse(sw.ReadLine());
+                
+            }
+            if(myint == 31)
+            {
+                Console.WriteLine("Quarters");
+            }
+        }
+    }
+}
