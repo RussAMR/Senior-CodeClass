f52807af466c4b91916358cca8e859962bc9fb8c
RussAMR <195712@student.asd20.org>
2019-02-13 13:38:14 -0700

    Started LinQ test

--- Semester 2/Algorith_Practice_Sec3/Algorith_Practice_Sec3/Program.cs
+++ Semester 2/Algorith_Practice_Sec3/Algorith_Practice_Sec3/Program.cs
@@ -10,6 +10,53 @@ namespace Algorith_Practice_Sec3
     {
         static void Main(string[] args)
         {
+
+        }
+        static void Decrypt()
+        {
+            List<int> Cypher = new List<int>();
+            List<char> Secrettext = new List<char>();
+            List<char> Jailbreak = new List<char>();
+            char Caesar;
+            int PreCypher;
+            string Userinput;
+            int Shiftnum;
+
+            Console.WriteLine("Hello welcome to the caesar cypher reconverter");
+            Console.WriteLine("Please input your shift # from -25 to 25");
+            Shiftnum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please input your Phrase");
+            Userinput = Console.ReadLine();
+            for (int I = 0; I <= Userinput.Length - 1; I++)
+            {
+                PreCypher = Userinput[I];
+                Cypher.Add(PreCypher - Shiftnum);
+            }
+            for (int I = 0; I <= Userinput.Length - 1; I++)
+            {
+                if (Cypher[I] >= 123)
+                {
+                    Cypher[I] = Cypher[I] - 26;
+                }
+                else if (Cypher[I] <= 96)
+                {
+                    Cypher[I] = Cypher[I] + 26;
+                }
+                else
+                {
+
+                }
+            }
+            for (int X = 0; X <= Userinput.Length - 1; X++)
+            {
+                Caesar = (char)Cypher[X];
+                Secrettext.Add(Caesar);
+
+            }
+            for (int P = 0; P <= Userinput.Length - 1; P++)
+            {
+                Console.Write(Secrettext[P]);
+            }
         }
     }
 }
---
+++ Semester 2/LinQ test/LinQ test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> L1 = new List<int>() { 5, 5, 5, 5, 3, 2, 9 };
+            List<int> L2 = new List<int>() { 3, 2, 2, 8, 0, 4, 7 };
+            List<int> L3 = new List<int>();
+
+            L3 = L1.Intersect(L2).ToList();
+
+
+            List<int> D1 = new List<int>() {8,6,7,5,3,0,9 };
+            List<int> D2 = new List<int>();
+            D2 = D1.Where(t => t >= 5 == true).ToList();
+            for (int i = 0; i < D2.Count; i++)
+            {
+                Console.WriteLine(D2[i]);
+            }
+
+            L3 = L1.TakeWhile(t => t >= 5).ToList();
+            
+            foreach(int l in L3)
+            {
+                Console.Write(l);
+            }
+        }
+
+    }
+}
