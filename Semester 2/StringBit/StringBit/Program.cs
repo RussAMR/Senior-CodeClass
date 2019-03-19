d9c28385f1c73b9dce0639785ccde8d1ca6fd302
RussAMR <195712@student.asd20.org>
2019-03-19 12:17:16 -0600

    String bit.

---
+++ Semester 2/StringBit/StringBit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string holder = "Hello";
+            for (int i = 0; i < holder.Length/2; i++)
+            {
+                Console.WriteLine(holder[counter]);
+              counter = counter + 2;
+            }
+        }
+    }
+}
