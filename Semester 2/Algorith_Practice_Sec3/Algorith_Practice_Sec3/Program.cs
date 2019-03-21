792f27a0d3a95c4ba2cea088142c455aff50416f
RussAMR <195712@student.asd20.org>
2019-03-21 14:43:37 -0600

    Completed section 3

--- Semester 2/Algorith_Practice_Sec3/Algorith_Practice_Sec3/Program.cs
+++ Semester 2/Algorith_Practice_Sec3/Algorith_Practice_Sec3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Algorith_Practice_Sec3
 {
@@ -10,53 +11,44 @@ namespace Algorith_Practice_Sec3
     {
         static void Main(string[] args)
         {
-
+           Decrypt();
         }
         static void Decrypt()
         {
-            List<int> Cypher = new List<int>();
+            List<string> Cypher = new List<string>();
             List<char> Secrettext = new List<char>();
-            List<char> Jailbreak = new List<char>();
-            char Caesar;
-            int PreCypher;
-            string Userinput;
-            int Shiftnum;
-
+                string line;
+            int holder;
+            string hold;
+            string path;
             Console.WriteLine("Hello welcome to the caesar cypher reconverter");
             Console.WriteLine("Please input your shift # from -25 to 25");
-            Shiftnum = int.Parse(Console.ReadLine());
             Console.WriteLine("Please input your Phrase");
-            Userinput = Console.ReadLine();
-            for (int I = 0; I <= Userinput.Length - 1; I++)
+            path = AppDomain.CurrentDomain.BaseDirectory + (@"Prob03.in_.txt");
+            using (StreamReader sr = new StreamReader(path))
             {
-                PreCypher = Userinput[I]; n
-                Cypher.Add(PreCypher - Shiftnum);
-            }
-            for (int I = 0; I <= Userinput.Length - 1; I++)
-            {
-                if (Cypher[I] >= 123)
+                while ((line = sr.ReadLine()) != null)
                 {
-                    Cypher[I] = Cypher[I] - 26;
-                }
-                else if (Cypher[I] <= 96)
-                {
-                    Cypher[I] = Cypher[I] + 26;
-                }
-                else
-                {
+
+                    Cypher.Add(line);
 
                 }
             }
-            for (int X = 0; X <= Userinput.Length - 1; X++)
+            for (int X = 1; X <= Cypher.Count - 1; X++)
             {
-                Caesar = (char)Cypher[X];
-                Secrettext.Add(Caesar);
+                string[] array = Cypher[X].Split('-', ' ');
+                for (int Z = 0; Z < array.Length; Z++)
+                {
+                    Secrettext.Add(Cypher[0][int.Parse(array[Z]) - 1]);
+                }
+                for (int P = 0; P <= Secrettext.Count - 1; P++)
+                {
+                    Console.Write(Secrettext[P]);                    
+                }
+                Secrettext.Clear();
+                Console.WriteLine();
+            }
 
-            }
-            for (int P = 0; P <= Userinput.Length - 1; P++)
-            {
-                Console.Write(Secrettext[P]);
-            }
         }
     }
 }
