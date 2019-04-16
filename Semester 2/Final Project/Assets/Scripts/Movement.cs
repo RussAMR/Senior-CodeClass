aa5dd56e922f70454ec8e3b9711a863bf3557845
RussAMR <195712@student.asd20.org>
2019-04-16 14:27:07 -0600

    Final Project push

---
+++ Semester 2/Final Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        if(Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Start")
+        {
+            SceneManager.LoadScene("LV1");
+        }
+    }
+}
