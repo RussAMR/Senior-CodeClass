a485c0a639aeccb3cd6a945955e5a32f79b02c4c
RussAMR <195712@student.asd20.org>
2019-05-03 14:40:31 -0600

    Almost finished Menu Scene.

---
+++ Semester 2/Unity Projects/Final Project/Assets/Scripts/TextBox_Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextBox_Menu : MonoBehaviour
+{
+    float timer = 0;
+    float time = 1;
+    int counter = 0;
+    GameObject Textmeshpro;
+    public TextMeshProUGUI textbox;
+    List<string> text = new List<string>() { "The Start button can be found in the bottom right." };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += time * Time.deltaTime;
+
+        if (timer >= 4)
+        {
+            if (counter < text.Count)
+            {
+                textbox.text = text[counter];
+                counter++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            timer = 0;
+        }
+    }
+}
